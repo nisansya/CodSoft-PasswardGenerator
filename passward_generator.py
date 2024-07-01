7453bf69abd09ec16b9d8ad6654e4bd48dbb40a9
nisansya <nisansya.kaul@gmail.com>
2024-07-01 17:40:06 +0530

    Update password_generator.py

--- passward_generator.py
+++ passward_generator.py
@@ -1,14 +1,14 @@
 import random
 import string
-def generate_passward(length):
+def generate_password(length):
     letters=string.ascii_letters
     digits=string.digits
     punc=string.punctuation
     alphabet=letters+digits+punc
-    random_passward=random.sample(alphabet,length)
-    passward="".join(random_passward)
-    print(passward)
+    random_password=random.sample(alphabet,length)
+    password="".join(random_password)
+    print(password)
 
-print("-------------PASSWARD GENERATOR-------------")
-length=int(input("Enter the length of the passward :"))
-generate_passward(length)
+print("-------------PASSWORD GENERATOR-------------")
+length=int(input("Enter the length of the password :"))
+generate_password(length)
